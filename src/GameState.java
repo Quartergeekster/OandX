6bb186e71e4f3b15d59aa8b374593e24d1b6d30a
Hawkeye Roe <r.roe@live.co.uk>
2016-06-14 15:38:07 +0100

    First commit

---
+++ src/GameState.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Robert on 14/06/2016.
+ */
+public enum GameState {
+    PLAYING, DRAW, X_WON, O_WON
+}
---
+++ src/Seed.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Robert on 14/06/2016.
+ */
+public enum Seed {
+    EMPTY, CROSS, NOUGHT
+}
