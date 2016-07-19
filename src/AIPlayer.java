24e96823b371cf1ca835a4897ca5d794dd8bb262
Hawkeye Roe <r.roe@live.co.uk>
2016-07-19 20:09:35 +0100

    AI added ArrayList idea

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * Created by Robert on 18/07/2016.
  */
@@ -6,18 +9,20 @@ public class AIPlayer {
     public int col;
     public int row;
 
+
     public void ReadBoard(Board board) {
-        outerloop:
+        ArrayList<ArrayList<Integer>> EmptyCells = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
+            ArrayList<Integer> SingleCell = new ArrayList<>();
             for (int j = 0; j < 3; j++) {
                 if (board.cells[i][j].content == Seed.EMPTY) {
                     System.out.println("Empty space found");
-                    row = i;
-                    col = j;
-                    break outerloop;
+                    SingleCell.add(j);
                 }
             }
+            EmptyCells.add(SingleCell);
         }
+        System.out.print(EmptyCells);
     }
 
     public void AIMove(Board board){
